6dcf18178fd62b75c48b37f9b77f9800000f7c6e
sethsaperstein <sethsaper@gmail.com>
2020-09-14 22:52:59 -0400

    test change

--- src/main.tf
+++ src/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "bexh"
+
+    workspaces {
+      name = "bexh-backend-aws-terraform"
+    }
+  }
+}
+
 provider "aws" {
     region = "us-east-1"
 }
@@ -99,7 +110,7 @@ resource "aws_lambda_function" "bexh_api_proxy_post" {
   s3_bucket = "bexh-lambda-deploy-develop-189266647936"
   s3_key = "bexh-api-aws-lambda.zip"
   s3_object_version = var.bexh_api_lambda_s3_version
-  function_name = "bexh-api-proxy-post"
+  function_name = "exh-api-proxy-post"
   role          = aws_iam_role.bexh_api_proxy_post_lambda_role.arn
   handler       = "main.src.service.handler"
   runtime       = "python3.8"

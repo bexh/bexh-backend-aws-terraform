dc5566517ac6f158feb72c7380409ac442391854
sethsaperstein <sethsaper@gmail.com>
2020-09-26 09:04:27 -0400

    fix output name

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,11 +1,11 @@
-output "API Gateway base_url" {
+output "base_url" {
   value = "${aws_api_gateway_deployment.this.invoke_url}"
 }
 
-output "ElasticSearch Endpoint" {
+output "es_endpoint" {
   value = "${aws_elasticsearch_domain.es.endpoint}"
 }
 
-output "ElasticSearch Kibana Endpoint" {
+output "kibana_endpoint" {
   value = "${aws_elasticsearch_domain.es.kibana_endpoint}"
 }

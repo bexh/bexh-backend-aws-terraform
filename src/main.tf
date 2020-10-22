ecdb7100455eaffbc4ea33d75d3cf49868fba7cb
sethsaperstein <sethsaper@gmail.com>
2020-10-22 00:51:15 -0400

    add sns topic policy to invoke lambda

--- src/main.tf
+++ src/main.tf
@@ -315,6 +315,52 @@ resource "aws_sns_topic" "email_topic" {
   name = "bexh-email-handler-topic"
 }
 
+resource "aws_sns_topic" "this" {
+  arn = aws_sns_topic.email_topic.arn
+
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      # "SNS:SetTopicAttributes",
+      # "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      # "SNS:ListSubscriptionsByTopic",
+      # "SNS:GetTopicAttributes",
+      # "SNS:DeleteTopic",
+      # "SNS:AddPermission",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        data.aws_caller_identity.current.account_id,
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      aws_sns_topic.email_topic.arn,
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
+
 resource "aws_lambda_permission" "email_invoke_function" {
   statement_id  = "AllowSnsInvoke"
   action        = "lambda:InvokeFunction"

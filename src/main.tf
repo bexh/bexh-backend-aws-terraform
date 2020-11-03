5b8108568b5b6656fd227cfcb3ddce774b6db024
sethsaperstein <sethsaper@gmail.com>
2020-11-02 20:48:45 -0500

    alpha num db name

--- src/main.tf
+++ src/main.tf
@@ -63,7 +63,7 @@ resource "aws_rds_cluster" "this" {
   engine                 = "aurora-mysql"
   engine_version         = "5.7.mysql_aurora.2.07.2"
   cluster_identifier     = "bexh-ods-cluster-${var.env_name}-${data.aws_caller_identity.current.account_id}"
-  database_name          = "bexh-ods-db-${var.env_name}-${data.aws_caller_identity.current.account_id}"
+  database_name          = "BexhOdsDb"
   master_username        = local.db_creds.username
   master_password        = local.db_creds.password
   db_cluster_parameter_group_name   = "default.aurora-mysql5.7"

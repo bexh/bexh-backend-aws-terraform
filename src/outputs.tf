817cabfbf014a8d3cace12faf3d7ca9d2bc8e634
Seth Saperstein <42792771+sethsaperstein@users.noreply.github.com>
2020-11-08 20:40:35 -0500

    push up old tf with final snapshot identifier for deletion (#1)

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,11 +1,11 @@
 output "API_Gateway_base_url" {
-  value = module.bexh_app.aws_api_gateway_deployment.invoke_url
+  value = aws_api_gateway_deployment.this.invoke_url
 }
 
 # output "ElasticSearch_Endpoint" {
-#   value = module.bexh_app.aws_elasticsearch_domain.es.endpoint
+#   value = aws_elasticsearch_domain.es.endpoint
 # }
 
 # output "ElasticSearch_Kibana_Endpoint" {
-#   value = module.bexh_app.aws_elasticsearch_domain.es.kibana_endpoint
+#   value = aws_elasticsearch_domain.es.kibana_endpoint
 # }

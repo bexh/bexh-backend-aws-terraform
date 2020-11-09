14983a7790bd074d006de86ff8c5c068468d2bbf
sethsaperstein <sethsaper@gmail.com>
2020-11-08 20:18:47 -0500

    move bexh app into module now that exchange exists

---
+++ src/modules/bexh_app/outputs.tf
@@ -0,0 +1,11 @@
+output "aws_api_gateway_deployment" {
+  value = aws_api_gateway_deployment.this
+}
+
+# output "ElasticSearch_Endpoint" {
+#   value = aws_elasticsearch_domain.es.endpoint
+# }
+
+# output "ElasticSearch_Kibana_Endpoint" {
+#   value = aws_elasticsearch_domain.es.kibana_endpoint
+# }
--- src/outputs.tf
+++ src/outputs.tf
@@ -1,11 +1,11 @@
 output "API_Gateway_base_url" {
-  value = aws_api_gateway_deployment.this.invoke_url
+  value = module.bexh_app.aws_api_gateway_deployment.invoke_url
 }
 
 # output "ElasticSearch_Endpoint" {
-#   value = aws_elasticsearch_domain.es.endpoint
+#   value = module.bexh_app.aws_elasticsearch_domain.es.endpoint
 # }
 
 # output "ElasticSearch_Kibana_Endpoint" {
-#   value = aws_elasticsearch_domain.es.kibana_endpoint
+#   value = module.bexh_app.aws_elasticsearch_domain.es.kibana_endpoint
 # }
